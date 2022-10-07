a8faf7a0be0bdd26b06e4aa91200bfc71a42f0e1
Edgar Veress <veress.edgar@yahoo.com>
2022-10-07 21:58:17 +0100

    finished

--- script.js
+++ script.js
@@ -1,42 +1,72 @@
 const box = document.querySelector('.box');
+const box2 = document.querySelector('.box2');
 const buttons = document.querySelectorAll('.btn');
+const btnA = document.getElementById('a');
+const btnB = document.getElementById('b');
+const btnC = document.getElementById('c');
 
-function randomRGB () {
+function randomRGB() {
     let r = Math.floor(Math.random() * 100);
     let g = Math.floor(Math.random() * 100);
     let b = Math.floor(Math.random() * 100);
     let rgb = `rgb(${r}, ${g}, ${b})`;
     return rgb;
 }
-const savedColor = randomRGB()
-box.style.backgroundColor = savedColor;
-box.innerHTML = savedColor;
+let savedColor = '';
 
-function shuffleBtns() {
-    a = Math.floor(Math.random() * 100 < 4);
-    b = Math.floor(Math.random() * 100 < 4);
-    c = Math.floor(Math.random() * 100 < 4);
-    
-    // if (a > b) return "a";
-    // if (b > a) return "b";
-    // if (a > c) return "a";
-    // if (c > a) return "c";
-    // if (b > c) return "b";
-    // if (c > b) return "c";
-    if (a > b) console.log("a");
-    if (b > a) console.log("b");
-    if (a > c) console.log("a");
-    if (c > a) console.log("c");
-    if (b > c) console.log("b");
-    if (c > b) console.log("c");
-}
+function initGame() {
+    buttons.forEach(btn => btn.disabled= false);
+    if (document.querySelector('.next-game')) {
+        document.querySelector('.next-game').remove();
+    }
+
+    savedColor = randomRGB();
+    box.style.backgroundColor = savedColor;
+    assignBtns();
+};
 
 function assignBtns() {
-    buttons.forEach(btn => {
-        btn.innerHTML = randomRGB();
-        let yolo = shuffleBtns();
-        document.getElementById(yolo).innerText = savedColor;
-    })
-}
+    let randomizedColors = [savedColor, randomRGB(), randomRGB()]
+    .sort(() => {return Math.random() - 0.5});
+    let btns = [btnA, btnB, btnC]
+    .sort(() => {return Math.random() - 0.5});
+    
+    for (let i = 0; i < 3; i++) {
+        btns[i].innerHTML = randomizedColors[i];
+    };
+};
 
-assignBtns()
+buttons.forEach(btn => {
+    const nextGame = document.createElement('button')
+    nextGame.classList.add('next-game')
+    nextGame.innerHTML = `Next Game`;
+    
+    btn.addEventListener('click', () => {
+        if (btn.innerHTML === savedColor) {
+            btn.style.backgroundColor = 'green';
+            box2.innerHTML = 'Winner!';
+        } else {
+            btn.style.backgroundColor = 'red';
+            box2.innerHTML = 'Sorry, try again';
+            let btns = [btnA, btnB, btnC]
+            btns.forEach(btn => {
+                if(btn.innerHTML === savedColor) {
+                    btn.style.backgroundColor = 'green';
+                }
+            })
+        };
+        document.querySelector('body').appendChild(nextGame);
+        buttons.forEach(btn => btn.disabled="true");
+        document.querySelector('.next-game').addEventListener('click', () => {
+            savedColor = '';
+            box.style.backgroundColor = '';
+            box2.innerHTML = '';
+            buttons.forEach(btn => {
+                btn.style.backgroundColor = '';
+            });
+            initGame();
+        });
+    });
+});
+
+initGame();
